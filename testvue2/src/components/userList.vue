941d8871a62ff71b3288615c56155c35c022c43e
1425918769@qq.com <1425918769@qq.com>
2022-04-19 11:19:54 +0800

    testvue2

---
+++ testvue2/src/components/userList.vue
@@ -0,0 +1,49 @@
+<template>
+    <ul class="todo-main">
+        <userItem 
+           v-for="todoObj in todos" 
+           :key="todoObj.id" 
+           :todo="todoObj" 
+           :checkTodo="checkTodo"
+           :deleteTodo="deleteTodo"
+
+
+        />
+
+    </ul>
+  
+</template>
+
+<script>
+    import userItem from './userItem.vue'
+
+    export default {
+        name:'userList',
+        components: {
+            userItem
+        },
+        props:['todos','checkTodo','deleteTodo']
+
+    
+
+    }
+</script>
+
+<style scoped>
+/*main*/
+  .todo-main {
+    margin-left: 0px;
+    border: 1px solid #ddd;
+    border-radius: 2px;
+    padding: 0px;
+  }
+
+  .todo-empty {
+    height: 40px;
+    line-height: 40px;
+    border: 1px solid #ddd;
+    border-radius: 2px;
+    padding-left: 5px;
+    margin-top: 10px;
+  }
+</style>
